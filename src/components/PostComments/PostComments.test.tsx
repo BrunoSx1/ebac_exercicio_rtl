f122e666335f35aadfdd9785996476ce93e3fd79
Bruno Silva <s.bruno.12@hotmail.com>
2023-08-24 07:11:19 -0300

    Adicionando 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
+import userEvent from '@testing-library/user-event';
+
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+    test('inserir dois comentários', () => {
+        render(<Post />);
+        
+        const textarea = screen.getByTestId('post-comments-form-textarea');
+        const button = screen.getByTestId('post-comments-form-button');
+        userEvent.type(textarea, 'Este é o primeiro comentário');
+        fireEvent.click(button);
+    
+        const comentario1 = screen.getByText('Este é o primeiro comentário');
+        expect(comentario1).toBeInTheDocument();
+    
+        userEvent.type(textarea, 'Este é o segundo comentário');
+        fireEvent.click(button);
+    
+        const comentario2 = screen.getByText('Este é o segundo comentário');
+        expect(comentario2).toBeInTheDocument();
+      });
+    });
